0cea91a0d02b14ccdb2442546bc6420f37008c66
jessicaescobedo <jessicaivetteescobedo@gmail.com>
2018-07-11 23:19:28 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -7,3 +7,16 @@ nickels = (cents)/5
 pennies = (cents)/1 
 end
 puts least_coins(8)
+
+
+
+def least_coins(cents)
+  if cents % 25 == 0 
+ puts ""
+  elsif a % 5 == 0
+    puts "buzz"
+  else  a % 3 == 0 
+    puts "fizz"
+ end
+end
+fizzbuzz(2)

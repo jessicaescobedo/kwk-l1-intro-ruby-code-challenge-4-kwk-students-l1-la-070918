f158a48a3fe559ace30eb75360e1e05247c88de8
jessicaescobedo <jessicaivetteescobedo@gmail.com>
2018-07-11 23:03:52 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,9 +1,9 @@
 #write out your code here
 
 def least_coins(cents)
-quarters = (cents)/25 
-dimes = (cents)/10
-nickels = (cents)/5
+:quarters = (cents)/25 
+:dimes = (cents)/10
+:nickels = (cents)/5
 pennies = (cents)/1 
 end
 puts least_coins(29)
